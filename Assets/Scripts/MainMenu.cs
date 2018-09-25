db8cd1f74b69238fee2ec08626658cd8c0e2b8c4
Juuso <juusoetela@gmail.com>
2018-09-25 08:37:09 +0300

    Basic menu, soundmanager and possible sfx

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public AudioClip onPress;
+
+
+    /// <summary>
+    /// Load the first scene
+    /// </summary>
+    public void StartGame()
+    {
+        SoundManager.Instance.PlaySfx(onPress);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Set volume to used audio sources
+    /// </summary>
+    /// <param name="volume"></param>
+    public void SetVolume( float volume)
+    {
+        SoundManager.Instance.sfxSource.volume = volume;
+        SoundManager.Instance.musicSource.volume = volume;
+    }
+
+    /// <summary>
+    /// Quit the app
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager Instance = null;
+
+    public AudioSource sfxSource;
+    public AudioSource musicSource;
+
+    // Initialize singleton instance.
+    private void Awake()
+    {
+        // If there is not already an instance of SoundManager, set it to this.
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        //If an instance already exists, destroy whatever this object is to enforce the singleton.
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void PlaySfx(AudioClip clip)
+    {
+        sfxSource.clip = clip;
+        sfxSource.Play();
+
+    }
+
+    public void PlayMusic(AudioClip musicClip)
+    {
+        musicSource.clip = musicClip;
+
+        musicSource.Play();
+        musicSource.loop = true;
+    }
+
+}
