38370b33fb20c0d4da6c78de27e5bd519effce53
Juuso <juusoetela@gmail.com>
2018-10-05 09:39:00 +0300

    Pause menu, particles

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    /// <summary>
+    /// Resumes the game
+    /// </summary>
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    /// <summary>
+    /// Stops the game
+    /// </summary>
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    /// <summary>
+    /// Load main menu scene
+    /// </summary>
+    public void QuitGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+
+        //Application.Quit();
+    }
+}
