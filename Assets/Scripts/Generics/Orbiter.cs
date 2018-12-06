6f4e835c2abf4d35a8204b05bab455281451154e
Juuso <juusoetela@gmail.com>
2018-12-06 16:19:28 +0200

    Planet selection menu 3d portion, selectable planet groups.

---
+++ Assets/Scripts/Generics/Orbiter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 axis;
+    public float speed;
+
+    private void Update()
+    {
+        transform.RotateAround(target.position, axis, speed*Time.deltaTime);
+        
+    }
+}
