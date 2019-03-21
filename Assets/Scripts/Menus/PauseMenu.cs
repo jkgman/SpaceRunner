4ec866e3180f68692496bf2dd16d9eb79c7b30e8
Juuso <juusoetela@gmail.com>
2019-03-21 13:31:39 +0200

    bugfixes

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -32,6 +32,7 @@ public class PauseMenu : MonoBehaviour {
     public void QuitGame()
     {
         Time.timeScale = 1;
+        GameManager.Instance.itemSlots = null;
         SceneManager.LoadScene(0);
 
         //Application.Quit();
